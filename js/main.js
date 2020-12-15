ac02b7e18b306af5f35e1894479f61a05a6e868e
Samule Ghurry <samule.ghurry@coxauto.co.uk>
2020-12-15 16:53:37 +0000

    refactor

--- js/main.js
+++ js/main.js
@@ -1,43 +1,42 @@
 function init() {
 	addFrameset();
-	addGestures();
+	addGestures(document);
 }
 
-function addGestures() {
-	document.addEventListener("swiped", function (e) {
-		console.log(e.type);
+function addGestures(element) {
+	element.addEventListener("swiped", function (e) {
+		/*console.log(e.type);
 		console.log(e.target);
 		console.log(e.detail);
-		console.log(e.dir);
-		alert(e.type);
+		console.log(e.dir);	*/
 	});
 
-	document.addEventListener("swiped-left", function (e) {
-		console.log(e.type);
+	element.addEventListener("swiped-left", function (e) {
+		/*console.log(e.type);
 		console.log(e.target);
 		console.log(e.detail);
-		alert(e.type);
+		alert(e.type);*/
 	});
 
-	document.addEventListener("swiped-right", function (e) {
-		console.log(e.type);
+	element.addEventListener("swiped-right", function (e) {
+		/*console.log(e.type);
 		console.log(e.target);
 		console.log(e.detail);
-		alert(e.type);
+		alert(e.type);*/
 	});
 
-	document.addEventListener("swiped-up", function (e) {
-		console.log(e.type);
+	element.addEventListener("swiped-up", function (e) {
+		/*console.log(e.type);
 		console.log(e.target);
 		console.log(e.detail);
-		alert(e.type);
+		alert(e.type);*/
 	});
 
-	document.addEventListener("swiped-down", function (e) {
-		console.log(e.type);
+	element.addEventListener("swiped-down", function (e) {
+		/*console.log(e.type);
 		console.log(e.target);
 		console.log(e.detail);
-		alert(e.type);
+		alert(e.type);*/
 	});
 }
 
